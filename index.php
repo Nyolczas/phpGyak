3f9aa3b38c55bb6194b7e264f3354a065e621e87
Nyolczas <nyolczas@gmail.com>
2019-04-05 14:16:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
+// =================== kávékülönlegességek ===================
 $kavekul = array(
     array('Kelet varázsa',
         '(kardamomos macchiato)',
         450),
+
     array('Juharszirupos Latte',
         '',
         590),
+
     array('Kókuszos tejeskávé tejszínhabbal',
         '(házi kókusz sziruppal)',
         650),
+
     array('Karamellás Latte tejszínhabbal',
         '(házi karamell sziruppal és öntettel)',
         680),
+
     array('Nana kedvence',
         '(habos kakaó kávéval)',
         650),
+
     array('Mogyorókrémes Latte tejszínhabbal',
         '(nagy adag Nutellával és házi csokiöntettel)',
         730),
+
     array('Növényi tej',
         '',
         '+ 100'),
@@ -27,11 +34,11 @@ $kavekul = array(
 foreach ($kavekul as $kave) {
     echo
         '<hr>
-              <div class="row">
-                <div class="col-9 balrazar">
-                  <p class="menu-tetel">' . $kave[0] . '</p>
-                  <p class="menu-magyarazat">' . $kave[1] . '</p>
-                </div>
-                <div class="col-3 jobbrazar">' . $kave[2] . '</div>
-              </div>';
+        <div class="row">
+            <div class="col-9 balrazar">
+                <p class="menu-tetel">' . $kave[0] . '</p>
+                <p class="menu-magyarazat">' . $kave[1] . '</p>
+            </div>
+            <div class="col-3 jobbrazar">' . $kave[2] . '</div>
+        </div>';
 }
